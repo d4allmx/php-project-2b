506a17ca2ae8273d51066fba135c3ccae017e1f3
d4all 3 <d4all3@MacBook-Air-2.local>
2019-01-15 15:34:34 -0700

    feature/choose-us: Finished block choose us (1/3) in all breakpoints.

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <?php include('inc/html.head.inc'); ?>
     <title>HomePage | PHP Project 2 B</title>
   </head>
-
   <body>
     <?php include('inc/three-columns.inc/three-columns.inc'); ?>
     <?php include('inc/welcome/welcome.inc'); ?>
     <?php include('inc/best-offers/best-offers.inc'); ?>
+    <?php include('inc/best-offers/choose-us.inc'); ?>
     <?php include('inc/footer.inc'); ?>
   </body>
 </html>
