6e39a96f0a1f656a23198bbafb3554600ca301bb
Alejandro Ponce <alexpglez.17@gmail.com>
2019-01-15 14:56:40 -0600

    feature/three-columns: Added styles on three columns.

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('inc/html.head.inc'); ?>
+    <title>Contact | PHP Project 2 B</title>
+  </head>
+
+  <body>
+    <?php include('inc/header.inc'); ?>
+    <?php include('inc/contact/contact.inc'); ?>
+    <?php include('inc/footer.inc'); ?>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -6,7 +6,6 @@
   </head>
 
   <body>
-    <?php include('inc/header.inc'); ?>
     <?php include('inc/three-columns.inc/three-columns.inc'); ?>
     <?php include('inc/welcome/welcome.inc'); ?>
     <?php include('inc/best-offers/best-offers.inc'); ?>
