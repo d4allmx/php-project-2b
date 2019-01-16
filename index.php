e8b5d7f1f04f82d8ffda649e0a94279b05d0be2f
Norma <norma.abr23@gmail.com>
2019-01-16 10:12:57 -0700

    feature/header: Added includes for header and footer.

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <title>HomePage | PHP Project 2 B</title>
   </head>
   <body>
+    <?php include('inc/global/header.inc'); ?>
     <?php include('inc/three-columns.inc/three-columns.inc'); ?>
     <?php include('inc/welcome/welcome.inc'); ?>
     <?php include('inc/best-offers/best-offers.inc'); ?>
     <?php include('inc/best-offers/choose-us.inc'); ?>
-    <?php include('inc/footer.inc'); ?>
+    <?php include('inc/global/footer.inc'); ?>
   </body>
 </html>
