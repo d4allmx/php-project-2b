52ff15feaff7f92e55a0455600155adefd5efc7b
montmadero <admin@d4all.mx>
2019-01-17 18:04:30 -0700

    feature/car-listing: Added all elements to the page.

---
+++ car-listing.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include('inc/html.head.inc'); ?>
+    <title>Pages | PHP Project 2 B</title>
+  </head>
+
+  <body>
+    <?php include('inc/car-listing/view-bar.inc'); ?>
+    <?php include('inc/car-listing/car-list.inc'); ?>
+    <?php include('inc/car-listing/pager.inc'); ?>
+    <?php include('inc/car-listing/filter-bar.inc'); ?>
+    <?php include('inc/car-listing/banner.inc'); ?>
+  </body>
+</html>
